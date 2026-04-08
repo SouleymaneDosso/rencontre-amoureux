d82838a17c904fe87314a923f26b781e58aec949
Dosso Souleymane <dosso070000@gmail.com>
2026-04-08 11:56:16 +0000

    m

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,43 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
+
 mongoose.connect(process.env.MONGOOSE_URL)
-.then(()=> console.log('connexion à la base de donnée réussie'))
-.catch(err =>{console.error("connexion échouée", err.message)
-    process.exit(1)
-})
+  .then(() => console.log("connexion à la base de donnée réussie"))
+  .catch(err => {
+    console.error("connexion échouée", err.message);
+    process.exit(1);
+  });
 
 app.use(cors({
-    origin:[
-        "http://localhost:5173",
-        "http://localhost:3000"
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://rencontre-amoureux.vercel.app"
+  ],
+  methods: ["GET", "PUT", "DELETE", "POST", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
-    ],
-    methods:["GET", "PUT", "DELETE", "POST","PATCH"],
-    allowedHeaders:["Content-Type", "Authorization"],
-    credentials: true
-}))
+app.use(express.json());
 
-app.use(express.json())
-
-const mesInfosRoute = require("./Router/index")
-const routerConnexion = require("./Router/connexion")
-const routerInscription = require("./Router/inscription")
+const mesInfosRoute = require("./Router/index");
+const routerConnexion = require("./Router/connexion");
+const routerInscription = require("./Router/inscription");
 const tchatRoutes = require("./Router/tchat");
 
-app.use("/api/mesInfos", mesInfosRoute)
-app.use("/api", routerConnexion)
-app.use("/api", routerInscription)
+app.use("/api/mesInfos", mesInfosRoute);
+app.use("/api", routerConnexion);
+app.use("/api", routerInscription);
 app.use("/api/tchat", tchatRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend rencontre-amoureux actif 🚀");
+});
+
 module.exports = app;
