246a2d22af4350d12cc6c1da7d6b8480e81ea6bf
Dosso Souleymane <dosso070000@gmail.com>
2026-04-10 01:34:57 +0000

    m

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import Profil from "./pages/profil";
 import Header from "./comportments/header";
@@ -16,46 +15,41 @@ import Decouverte from "./pages/decouverte";
 import Matchs from "./pages/match";
 import Conversations from "./pages/conversations";
 
-const AppWrapper = styled.div`
-  min-height: 100vh;
-  background: #f8fafc;
-`;
+// 🔥 Composant layout
+function Layout() {
+  const location = useLocation();
 
-const MainContent = styled.main`
-  padding-top: 110px;
-  padding-bottom: 90px;
+  // cacher header si on est sur /tchat
+  const hideHeader = location.pathname.startsWith("/tchat");
 
-  @media (max-width: 768px) {
-    padding-top: 100px;
-    padding-bottom: 100px;
-  }
-`;
+  return (
+    <>
+      {!hideHeader && <Header />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Home />} />
+        <Route path="/profil" element={<Profil />} />
+        <Route path="/tchat/:id" element={<Tchat />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/connexion" element={<Connexion />} />
+        <Route path="/modifier/:id" element={<Modifier />} />
+        <Route path="/profilpublic/:id" element={<Profilpublic />} />
+        <Route path="/decouverte" element={<Decouverte />} />
+        <Route path="/matchs" element={<Matchs />} />
+        <Route path="/conversations" element={<Conversations />} />
+        <Route path="*" element={<h1>Page non trouvée</h1>} />
+      </Routes>
+
+      <FooterNav />
+    </>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <AppWrapper>
-        <Header />
-
-        <MainContent>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:id" element={<Home />} />
-            <Route path="/profil" element={<Profil />} />
-            <Route path="/tchat/:id" element={<Tchat />} />
-            <Route path="/inscription" element={<Inscription />} />
-            <Route path="/connexion" element={<Connexion />} />
-            <Route path="/modifier/:id" element={<Modifier />} />
-            <Route path="/profilpublic/:id" element={<Profilpublic />} />
-            <Route path="/decouverte" element={<Decouverte />} />
-            <Route path="/matchs" element={<Matchs />} />
-            <Route path="/conversations" element={<Conversations />} />
-            <Route path="*" element={<h1>Page non trouvée</h1>} />
-          </Routes>
-        </MainContent>
-
-        <FooterNav />
-      </AppWrapper>
+      <Layout />
     </Router>
   </StrictMode>
 );
