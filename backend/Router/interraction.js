4700f0e14c2104f061abbd5cef88fc729852abc2
Dosso Souleymane <dosso070000@gmail.com>
2026-04-26 17:23:40 +0000

    m

--- backend/Router/interraction.js
+++ backend/Router/interraction.js
@@ -1,13 +1,12 @@
 const express = require("express");
-import Video from './../../frontend/src/pages/videos/index';
 const router = express.Router()
 const auth = require("../middleware/auth");
 const upload = require("../multer")
 
-const {uploadCloudinary, getvideo, getalldeo } = require("../logiquemetier/interraction");
+const {uploadCloudinary, getvideo, getalldeo} = require("../logiquemetier/interraction");
 
 router.post("/videos",auth, upload.array("video",10), uploadCloudinary)
 router.get("/mesvideos",auth, getvideo)
-router.get("/publicdeo", getalldeo)
+router.get("/videos/public", getalldeo)
 
 module.exports = router;
