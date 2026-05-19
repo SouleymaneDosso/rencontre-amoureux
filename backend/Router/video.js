fd618d3d30d181a140b7b2207e283e04a1337125
Dosso Souleymane <dosso070000@gmail.com>
2026-05-19 15:54:36 +0000

    m

--- backend/Router/video.js
+++ backend/Router/video.js
@@ -5,7 +5,7 @@ const upload = require("../multer")
 
 const videos = require("../logiquemetier/video");
 
-router.post("/videos",auth, upload.array("video",10), videos.uploadCloudinary)
+router.post("/videos",auth, upload.array("media",10), videos.uploadCloudinary)
 router.get("/mesvideos",auth, videos.getMyVideos)
 router.get("/videos/public", videos.getAllVideos)
 router.put("/likes/:videoId", auth, videos.likes)
