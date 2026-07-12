36ab4a9bf7291c775f999ea58f8f623e6ec79d28
Dosso Souleymane <dosso070000@gmail.com>
2026-07-12 19:08:16 +0000

    m

---
+++ frontend/src/components/hooks/useAudioCall.js
@@ -0,0 +1,140 @@
+import { useState,useRef,useEffect} from "react";
+import {
+  creerMessageAppel,
+} from "../../services/tchatApi";
+
+export default function useAudioCall({socket,
+    id,
+    token,
+    messages,
+    profilCible,
+    monProfilId}) {
+const localStreamRef = useRef(null);
+ const [calling, setCalling] = useState(false);
+  const [incomingCall, setIncomingCall] = useState(null);
+
+useEffect(() => {
+    const handleIncomingCall = ({ from }) => {
+      console.log("📞 Appel entrant de :", from);
+
+      setIncomingCall({
+        from,
+      });
+    };
+
+    socket.on("incomingCall", handleIncomingCall);
+
+    return () => {
+      socket.off("incomingCall", handleIncomingCall);
+    };
+  }, []);
+
+    useEffect(() => {
+    const handleRejected = () => {
+      setCalling(false);
+
+      alert("L'appel a été refusé.");
+    };
+
+    socket.on("callRejected", handleRejected);
+
+    return () => {
+      socket.off("callRejected", handleRejected);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleCancel = () => {
+      setIncomingCall(null);
+    };
+
+    socket.on("callCancelled", handleCancel);
+
+    return () => {
+      socket.off("callCancelled", handleCancel);
+    };
+  }, []);
+
+  const acceptCall = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+
+      localStreamRef.current = stream;
+
+      setIncomingCall(null);
+
+      console.log("🎤 Micro autorisé :", stream);
+    } catch (error) {
+      console.error("Accès au micro refusé :", error);
+    }
+  };
+const startCall = () => {
+    setCalling(true);
+
+    socket.emit("callUser", {
+      to: id,
+      from: {
+        id: monProfilId,
+        pseudo: profilCible?.pseudo,
+        avatar: profilCible?.avatar?.url,
+      },
+    });
+  };
+
+  const cancelCall = async () => {
+    setCalling(false);
+
+    try {
+      const message = await creerMessageAppel(token, {
+        conversationId: messages[0]?.conversationId,
+        destinataire: id,
+        status: "cancelled",
+      });
+
+      socket.emit("sendMessage", message);
+
+      socket.emit("cancelCall", {
+        to: id,
+        from: monProfilId,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const rejectCall = async () => {
+    if (!incomingCall) return;
+    try {
+      const message = await creerMessageAppel(token, {
+        conversationId: messages[0]?.conversationId,
+        destinataire: incomingCall.from.id,
+        status: "rejected",
+      });
+
+      socket.emit("sendMessage", message);
+
+      socket.emit("rejectCall", {
+        to: incomingCall.from.id,
+        from: monProfilId,
+      });
+
+      setIncomingCall(null);
+      setCalling(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
+  return{
+calling,
+incomingCall,
+acceptCall,
+rejectCall,
+cancelCall,
+startCall
+
+  }
+}
--- frontend/src/pages/tchat/index.jsx
+++ frontend/src/pages/tchat/index.jsx
@@ -8,6 +8,7 @@ import { FaPhoneSlash, FaPhone } from "react-icons/fa";
 import { MdCallMade, MdCallReceived } from "react-icons/md";
 import MessageItem from "../../components/chat/MessageItem";
 import HeaderChat from "../../components/chat/HeaderChat";
+import useAudioCall from "../../Components/hooks/useAudioCall";
 
 import { FaPhoneAlt } from "react-icons/fa";
 const API_URL = import.meta.env.VITE_API_URL;
@@ -39,7 +40,6 @@ import {
 import { useLocation } from "react-router-dom";
 
 import { FaExpand } from "react-icons/fa";
-
 
 const PreviewActions = styled.div`
   display: flex;
@@ -362,7 +362,6 @@ const StatusWrapper = styled.span`
   align-items: center;
   opacity: 0.9;
 `;
-
 
 const SkeletonBubble = styled.div`
   width: ${(props) => props.width || "60%"};
@@ -914,7 +913,7 @@ function Tchat() {
   const previewProgressRef = useRef(null);
   const audioRefs = useRef({});
   const previewAudioRef = useRef(null);
-  const localStreamRef = useRef(null);
+  
 
   const progressRefs = useRef({});
   const recordingStartRef = useRef(null);
@@ -967,9 +966,21 @@ function Tchat() {
   const [previewProgress, setPreviewProgress] = useState(0);
   const [previewCurrentTime, setPreviewCurrentTime] = useState(0);
   const [draggingPreview, setDraggingPreview] = useState(false);
-  const [calling, setCalling] = useState(false);
-  const [incomingCall, setIncomingCall] = useState(null);
-
+  const {
+    calling,
+    incomingCall,
+    startCall,
+    acceptCall,
+    rejectCall,
+    cancelCall,
+  } = useAudioCall({
+    socket,
+    id,
+    token,
+    monProfilId,
+    profilCible,
+    messages,
+  });
   // swipe pour messages
 
   const finSwipeReponse = (e, msg) => {
@@ -1055,26 +1066,6 @@ function Tchat() {
     }
   };
   // fin modal message
-
-  //appelles//
-
-  const acceptCall = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-      });
-
-      localStreamRef.current = stream;
-
-      setIncomingCall(null);
-
-      console.log("🎤 Micro autorisé :", stream);
-    } catch (error) {
-      console.error("Accès au micro refusé :", error);
-    }
-  };
-
-  // finAPPelle
 
   // audio
 
@@ -1344,24 +1335,6 @@ function Tchat() {
       setSending(false);
     }
   };
-
-  useEffect(() => {
-    const handleIncomingCall = ({ from }) => {
-      console.log("📞 Appel entrant de :", from);
-
-      setIncomingCall({
-        from,
-      });
-    };
-
-    socket.on("incomingCall", handleIncomingCall);
-
-    return () => {
-      socket.off("incomingCall", handleIncomingCall);
-    };
-  }, []);
-
-  // fin audio
 
   // modal zone
 
@@ -1921,88 +1894,6 @@ function Tchat() {
     }
   };
 
-  const startCall = () => {
-    setCalling(true);
-
-    socket.emit("callUser", {
-      to: id,
-      from: {
-        id: monProfilId,
-        pseudo: profilCible?.pseudo,
-        avatar: profilCible?.avatar?.url,
-      },
-    });
-  };
-
-  const cancelCall = async () => {
-    setCalling(false);
-
-    try {
-      const message = await creerMessageAppel(token, {
-        conversationId: messages[0]?.conversationId,
-        destinataire: id,
-        status: "cancelled",
-      });
-
-      socket.emit("sendMessage", message);
-
-      socket.emit("cancelCall", {
-        to: id,
-        from: monProfilId,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const rejectCall = async () => {
-    try {
-      const message = await creerMessageAppel(token, {
-        conversationId: messages[0]?.conversationId,
-        destinataire: incomingCall.from.id,
-        status: "rejected",
-      });
-
-      socket.emit("sendMessage", message);
-
-      socket.emit("rejectCall", {
-        to: incomingCall.from.id,
-        from: monProfilId,
-      });
-
-      setIncomingCall(null);
-      setCalling(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    const handleRejected = () => {
-      setCalling(false);
-
-      alert("L'appel a été refusé.");
-    };
-
-    socket.on("callRejected", handleRejected);
-
-    return () => {
-      socket.off("callRejected", handleRejected);
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleCancel = () => {
-      setIncomingCall(null);
-    };
-
-    socket.on("callCancelled", handleCancel);
-
-    return () => {
-      socket.off("callCancelled", handleCancel);
-    };
-  }, []);
-
   // supprimer messages
   const supprimemoi = async (messageId) => {
     try {
@@ -2116,7 +2007,7 @@ function Tchat() {
 
   return (
     <Wrapper>
-       {incomingCall && (
+      {incomingCall && (
         <CallingBanner>
           <CallerSection>
             <CallAvatar>
@@ -2138,7 +2029,7 @@ function Tchat() {
             <FaPhoneSlash />
           </EndCallButton>
           <Acceptbutton onClick={acceptCall}>
-            <FaPhone/>
+            <FaPhone />
           </Acceptbutton>
         </CallingBanner>
       )}
