affc51b1f69b8540fa23128c882e2fa7a88ca36e
Dosso Souleymane <dosso070000@gmail.com>
2026-07-22 01:59:48 +0000

    m

--- frontend/src/components/call/CallManager.jsx
+++ frontend/src/components/call/CallManager.jsx
@@ -1,5 +1,3 @@
-
-import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import { socket } from "../../socket";
@@ -9,32 +7,66 @@ import CallModal from "../../pages/tchat/CallModal";
 export default function CallManager() {
   const location = useLocation();
 
-  const [activeCall, setActiveCall] = useState(null);
-
-  // On récupère l'ID depuis l'URL
-  const currentUserId = location.pathname.startsWith("/tchat/")
+  // Récupère l'utilisateur de la conversation actuelle
+  const id = location.pathname.startsWith("/tchat/")
     ? location.pathname.split("/tchat/")[1]
     : null;
 
-  console.log("Utilisateur de la conversation :", currentUserId);
+  // Récupère ton profil depuis le localStorage
+  const monProfil = JSON.parse(localStorage.getItem("monProfil"));
+
+  const monProfilId = monProfil?._id || null;
+
+  const token = localStorage.getItem("token");
+
+  const {
+    calling,
+    incomingCall,
+    startCall,
+    acceptCall,
+    rejectCall,
+    cancelCall,
+    remoteAudioRef,
+    inCall,
+    endCall,
+    callDuration,
+    isMinimized,
+    minimizeCall,
+    maximizeCall,
+    isMuted,
+    toggleMute,
+  } = useAudioCall({
+    socket,
+    id,
+    token,
+    monProfilId,
+    profilCible: null,
+    messages: [],
+  });
 
   return (
     <>
+      <audio
+        ref={remoteAudioRef}
+        autoPlay
+        playsInline
+      />
+
       <CallModal
-        open={false}
-        incoming={false}
-        calling={false}
-        inCall={false}
+        open={incomingCall || calling || inCall}
+        incoming={!!incomingCall}
+        calling={calling}
+        inCall={inCall}
         profilCible={null}
-        onAccept={() => {}}
-        onReject={() => {}}
-        onCancel={() => {}}
-        callDuration={0}
-        isMinimized={false}
-        minimizeCall={() => {}}
-        maximizeCall={() => {}}
-        isMuted={false}
-        toggleMute={() => {}}
+        onAccept={acceptCall}
+        onReject={rejectCall}
+        onCancel={inCall ? endCall : cancelCall}
+        callDuration={callDuration}
+        isMinimized={isMinimized}
+        minimizeCall={minimizeCall}
+        maximizeCall={maximizeCall}
+        isMuted={isMuted}
+        toggleMute={toggleMute}
       />
     </>
   );
